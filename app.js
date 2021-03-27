5d165e576966c5570d7ed1075bb33c0ee8fcc53e
Qusai-Alnonah <kusaialnona@yahoo.com>
2021-03-27 21:42:14 +0300

    Add new site

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Welcome The User
+alert('Welcom TO Soical Media ');
 var userName = prompt('what is your name? ');
 alert('Welcome ' + userName);
-var NameSocial = prompt("What is your favorite Social Media Facebook ,Twitter,Github  ")
+var NameSocial = prompt("What is your favorite Social Media Facebook ,Twitter,Github ,Youtube ")
 if(NameSocial==='Facebook'){
     document.write('<img scr="https://www.facebook.com/images/fb_icon_325x325.png"/>')
 }else if (NameSocial==='Twitter'){
@@ -10,4 +11,8 @@ if(NameSocial==='Facebook'){
     document.write('<img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqFequSxunU1CSZiEE0WzIKTlMj7WqC_w3Hg&usqp=CAU"/>')
 }else {
     alert('You Are Welcome')
+};
+if (NameSocial==='Youtube'){
+    document.write(<img src = "https://pbs.twimg.com/profile_images/954305257898561536/k2qB-Igk_400x400.jpg"/>)
+}else {alert('Welcom')
 }
