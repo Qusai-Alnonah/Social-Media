469f1b5a4ba1d0de62784bb2bc65351c6294c8b5
Qusai-Alnonah <kusaialnona@yahoo.com>
2021-03-28 17:29:25 +0300

    add java loop code

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var userName = prompt('what is your name? ');
 alert('Welcome ' + userName);
 var NameSocial = prompt("What is your favorite Social Media Facebook ,Twitter,Github ,Youtube ")
 if(NameSocial==='Facebook'){
-    document.write('<img scr="https://www.facebook.com/images/fb_icon_325x325.png"/>')
+    document.write('<img src="https://www.facebook.com/images/fb_icon_325x325.png"/>')
 }else if (NameSocial==='Twitter'){
-    document.write('<img scr = "https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/300px-Twitter_Bird.svg.png"/>')
+    document.write('<img src = "https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/300px-Twitter_Bird.svg.png"/>')
 }else if(NameSocial==='Github'){
     document.write('<img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqFequSxunU1CSZiEE0WzIKTlMj7WqC_w3Hg&usqp=CAU"/>')
 }else {
@@ -16,7 +16,7 @@ if (NameSocial==='Youtube'){
     document.write(<img src = "https://pbs.twimg.com/profile_images/954305257898561536/k2qB-Igk_400x400.jpg"/>)
 }else {alert('Welcom')
 }
- confirm(message?: string): boolean)
+
 var QusaiACC =prompt('If you to see more desgin Write more')
 if (QusaiACC==='more'){
     document.write(<img src="https://qusai-alnonah.github.io/Social-Media/"/>)
---
+++ loops.js
@@ -0,0 +1,28 @@
+var nameSocial = prompt("What is your favorite Social Media Facebook ,Twitter,Github ,Youtube ");
+var userName = prompt("Pleas enter your username");
+while (userName !== "Qusai-Alnonah") {
+var userName = prompt("Pleas enter correct username");
+ }
+var Password = prompt("Pleas enter your Password");
+ while (Password !== "123456") {
+     var Password = prompt("The Password you inter is wrong reinter it pleas");
+ }
+var repPhoto = prompt("how many time you wont to see the photo for your favarite site");
+
+
+for (var i = 0; i <= repPhoto; i++) {
+    
+   
+    if (nameSocial === 'Facebook') {
+        document.write('<img src ="https://www.facebook.com/images/fb_icon_325x325.png"/>');
+    } else if (nameSocial === 'Twitter') {
+        document.write('<img src = "https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/300px-Twitter_Bird.svg.png"/>');
+    } else if (nameSocial === 'Github') {
+        document.write('<img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqFequSxunU1CSZiEE0WzIKTlMj7WqC_w3Hg&usqp=CAU"/>');
+    } else if (nameSocial === 'Youtube') {
+        document.write('<img src ="https://pbs.twimg.com/profile_images/954305257898561536/k2qB-Igk_400x400.jpg"/>');
+    }
+    else {
+        alert('You Are Welcome');
+    }
+}
